f7121de1cb44a38f6ff542562e49c6ffb414d9aa
root <1030935750@qq.com>
2020-09-06 14:38:40 +0800

    创建业务逻辑system模块

---
+++ system/src/main/java/com/course/system/SystemApp.java
@@ -0,0 +1,22 @@
+package com.course.system;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+/**
+ * Created by ZHX on 2020/9/6.
+ */
+@SpringBootApplication
+
+public class SystemApp {
+    private static final Logger LOG = LoggerFactory.getLogger(SystemApp.class);
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SystemApp.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("System地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
