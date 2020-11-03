dd3620bd5c784e033dcc363b688325d0e8b2455f
zhanghongxin <1030935750@qq.com>
2020-11-03 15:57:25 +0800

    增加事务配置

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,10 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement //事务注解
+@Configuration //表示是一个配置类
+public class TransactionManagementConfig {
+   
+}
