258cf477851f454073861e6c2e575ec38d876363
zhanghongxin <1030935750@qq.com>
2020-11-10 17:58:57 +0800

    增加讲师管理功能

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,10 +3,11 @@ import Router from "vue-router"
 import Login from "./views/login.vue"
 import Admin from "./views/admin.vue"
 import Welcome from "./views/admin/welcome.vue"
+import Category from "./views/admin/category.vue"
+import Course from "./views/admin/course.vue"
 import Chapter from "./views/admin/chapter.vue"
 import Section from "./views/admin/section.vue"
-import Course from "./views/admin/course.vue"
-import Category from "./views/admin/category";
+import Teacher from "./views/admin/teacher.vue"
 
 Vue.use(Router);
 
@@ -28,21 +29,25 @@ export default new Router({
             name: "welcome",
             component: Welcome,
         }, {
+            path: "business/category",
+            name: "business/category",
+            component: Category,
+        }, {
+            path: "business/course",
+            name: "business/course",
+            component: Course,
+        }, {
             path: "business/chapter",
             name: "business/chapter",
             component: Chapter,
         }, {
-            path: "business/category",
-            name: "business/category",
-            component: Category,
-            }, {
             path: "business/section",
             name: "business/section",
             component: Section,
-        },{
-            path: "business/course",
-            name: "business/course",
-            component: Course,
+        }, {
+            path: "business/teacher",
+            name: "business/teacher",
+            component: Teacher,
         }]
     }]
 })
